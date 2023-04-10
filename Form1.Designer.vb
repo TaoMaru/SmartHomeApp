998fb698c4bfa173e2188a82d8ae73ecedc7bb91
TaoMaru <jackson_maria1@columbusstate.edu>
2023-04-09 21:34:49 -0400

    Add text and styling for font and size. Add smarthome image

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,6 +22,7 @@ Partial Class frmSmartHome
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmSmartHome))
         Me.btnStats = New System.Windows.Forms.Button()
         Me.lblHeading = New System.Windows.Forms.Label()
         Me.cboMonth = New System.Windows.Forms.ComboBox()
@@ -34,20 +35,23 @@ Partial Class frmSmartHome
         '
         'btnStats
         '
-        Me.btnStats.Location = New System.Drawing.Point(254, 359)
+        Me.btnStats.Font = New System.Drawing.Font("Tahoma", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnStats.Location = New System.Drawing.Point(230, 380)
         Me.btnStats.Name = "btnStats"
-        Me.btnStats.Size = New System.Drawing.Size(326, 55)
+        Me.btnStats.Size = New System.Drawing.Size(375, 55)
         Me.btnStats.TabIndex = 0
-        Me.btnStats.Text = "Button1"
+        Me.btnStats.Text = "Display Statistics"
         Me.btnStats.UseVisualStyleBackColor = True
         '
         'lblHeading
         '
-        Me.lblHeading.Location = New System.Drawing.Point(423, 38)
+        Me.lblHeading.Font = New System.Drawing.Font("Tahoma", 26.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeading.Location = New System.Drawing.Point(434, 9)
         Me.lblHeading.Name = "lblHeading"
-        Me.lblHeading.Size = New System.Drawing.Size(389, 90)
+        Me.lblHeading.Size = New System.Drawing.Size(389, 126)
         Me.lblHeading.TabIndex = 1
-        Me.lblHeading.Text = "Label1"
+        Me.lblHeading.Text = "Smart Home" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Electric Savings"
+        Me.lblHeading.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'cboMonth
         '
@@ -59,33 +63,41 @@ Partial Class frmSmartHome
         '
         'lblStats
         '
-        Me.lblStats.Location = New System.Drawing.Point(151, 282)
+        Me.lblStats.Font = New System.Drawing.Font("Tahoma", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStats.Location = New System.Drawing.Point(44, 319)
         Me.lblStats.Name = "lblStats"
-        Me.lblStats.Size = New System.Drawing.Size(532, 58)
+        Me.lblStats.Size = New System.Drawing.Size(747, 58)
         Me.lblStats.TabIndex = 3
-        Me.lblStats.Text = "Label2"
+        Me.lblStats.Text = "The electric savings for XXXXXXXX was: $XXXX"
+        Me.lblStats.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'lblAvg
         '
-        Me.lblAvg.Location = New System.Drawing.Point(151, 438)
+        Me.lblAvg.Font = New System.Drawing.Font("Tahoma", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAvg.Location = New System.Drawing.Point(44, 447)
         Me.lblAvg.Name = "lblAvg"
-        Me.lblAvg.Size = New System.Drawing.Size(532, 58)
+        Me.lblAvg.Size = New System.Drawing.Size(747, 58)
         Me.lblAvg.TabIndex = 4
-        Me.lblAvg.Text = "Label3"
+        Me.lblAvg.Text = "The average monthly savings was: $XXXX"
+        Me.lblAvg.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'lblMost
         '
-        Me.lblMost.Location = New System.Drawing.Point(151, 514)
+        Me.lblMost.Font = New System.Drawing.Font("Tahoma", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMost.Location = New System.Drawing.Point(44, 526)
         Me.lblMost.Name = "lblMost"
-        Me.lblMost.Size = New System.Drawing.Size(532, 58)
+        Me.lblMost.Size = New System.Drawing.Size(747, 58)
         Me.lblMost.TabIndex = 5
-        Me.lblMost.Text = "Label4"
+        Me.lblMost.Text = "XXXXXXXX had the most significant monthly savings."
+        Me.lblMost.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'picSmartHome
         '
+        Me.picSmartHome.Image = CType(resources.GetObject("picSmartHome.Image"), System.Drawing.Image)
         Me.picSmartHome.Location = New System.Drawing.Point(0, 0)
         Me.picSmartHome.Name = "picSmartHome"
-        Me.picSmartHome.Size = New System.Drawing.Size(405, 267)
+        Me.picSmartHome.Size = New System.Drawing.Size(415, 295)
+        Me.picSmartHome.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.picSmartHome.TabIndex = 6
         Me.picSmartHome.TabStop = False
         '
